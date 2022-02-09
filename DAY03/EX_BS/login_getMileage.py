b8c60ea29f7eb6ac38c71570d26db2d06de0ef37
Administrator <qvvvvvv99@naver.com>
2022-02-09 20:26:41 +0900

    로그인이 필요한 사이트에서 다운받기

---
+++ DAY03/EX_BS/login_getMileage.py
@@ -0,0 +1,26 @@
+from bs4 import BeautifulSoup
+from urllib.parse import urljoin
+import requests
+
+USER = "anece1111"
+PASS = "so18051805!"
+
+session = requests.session()
+
+login_info = {
+    "m_id": USER,
+    "m_passwd": PASS
+}
+url_login="https://www.hanbit.co.kr/member/login_proc.php"
+res=session.post(url_login, data=login_info)
+res.raise_for_status()
+
+url_mypage="https://www.hanbit.co.kr/myhanbit/myhanbit.html"
+res=session.get(url_mypage)
+res.raise_for_status()
+
+soup=BeautifulSoup(res.text, "html.parser")
+mileage=soup.select_one(".mileage_section1 span").get_text()
+ecoin=soup.select_one(".mileage_section2 span").get_text()
+print("마일리지: "+mileage)
+print("이코인: "+ecoin)
